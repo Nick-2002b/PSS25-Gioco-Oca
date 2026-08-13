5b0c62dd523f506b8492cd315cc9c56d5e2ed991
Nick-2002b <nicolae.balaban@studio.unibo.it>
2026-08-13 15:24:45 +0200

    feat(model): implement Piece interface with PieceImpl record

---
+++ src/main/java/it/unibo/giocooca/model/impl/PieceImpl.java
@@ -0,0 +1,15 @@
+﻿package it.unibo.giocooca.model.impl;
+
+import it.unibo.giocooca.model.Piece;
+
+public record PieceImpl(String name, String color) implements Piece {
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+}
