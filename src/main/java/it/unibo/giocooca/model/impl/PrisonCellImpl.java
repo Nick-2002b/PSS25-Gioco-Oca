ce1121fca6fd3b7d77adc3b1e734da1f823f4fad
Nick-2002b <nicolae.balaban@studio.unibo.it>
2026-08-13 16:54:25 +0200

    feat(model): implement PrisonCellImpl class

---
+++ src/main/java/it/unibo/giocooca/model/impl/PrisonCellImpl.java
@@ -0,0 +1,28 @@
+package it.unibo.giocooca.model.impl;
+
+import it.unibo.giocooca.model.Cell;
+import it.unibo.giocooca.model.CellType;
+import it.unibo.giocooca.model.Player;
+
+public class PrisonCellImpl implements Cell {
+    private final int position;
+
+    public PrisonCellImpl(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public CellType getType() {
+        return CellType.PRISON;
+    }
+
+    @Override
+    public int getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public void applyEffect(Player player) {
+        player.setInPrison(true);
+    }
+}
