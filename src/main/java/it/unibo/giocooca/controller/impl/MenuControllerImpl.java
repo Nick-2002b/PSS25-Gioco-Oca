d9c7fa6106b58e45ba6a1cbca5dccac4ff987629
Nick-2002b <nicolae.balaban@studio.unibo.it>
2026-08-22 15:48:02 +0200

    feat(controller): enhance MenuControllerImpl to manage settings and audio

--- src/main/java/it/unibo/giocooca/controller/impl/MenuControllerImpl.java
+++ src/main/java/it/unibo/giocooca/controller/impl/MenuControllerImpl.java
@@ -1,24 +1,32 @@
 package it.unibo.giocooca.controller.impl;
 
+import it.unibo.giocooca.audio.SoundEffect;
+import it.unibo.giocooca.audio.SoundManager;
 import it.unibo.giocooca.controller.SetupController;
 import it.unibo.giocooca.controller.MenuController;
+import it.unibo.giocooca.model.Settings;
+import it.unibo.giocooca.model.impl.SettingsManager;
 import it.unibo.giocooca.view.MenuView;
 import it.unibo.giocooca.view.impl.MenuViewImpl;
-import it.unibo.giocooca.view.impl.SettingsViewImpl;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class MenuControllerImpl implements MenuController{
     private final Stage stage;
     private final MenuView view;
+    private final Settings settings;
+
 
     public MenuControllerImpl(Stage stage){
         this.stage = stage;
+        this.settings = new SettingsManager().load();
+        SoundManager.getInstance().setMusicVolume(settings.getMusicVolume());
+        SoundManager.getInstance().setSfxVolume(settings.getSfxVolume());
         this.view = new MenuViewImpl(stage, this);           
-        
     }
     @Override
     public void start(){
+        SoundManager.getInstance().playMusic(SoundEffect.BACKGROUND_MUSIC);
         this.view.show();
     }
 
@@ -30,7 +38,7 @@ public class MenuControllerImpl implements MenuController{
 
     @Override
     public void onOpenSettings() {
-        new SettingsViewImpl(stage, this).show();
+        new SettingsControllerImpl(stage, settings, this).show();
     }
 
     @Override
